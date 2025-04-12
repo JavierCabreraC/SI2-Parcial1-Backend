cbfa6e8def84fd67634cc4574a1a0f31db52f303
unknown <fcabreraclaros@gmail.com>
2025-04-11 23:38:50 -0400

    blobs

--- apps/productos/models.py
+++ apps/productos/models.py
@@ -1,3 +1,21 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+
+class Producto(models.Model):
+    codigo = models.CharField(max_length=50, unique=True)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_actual = models.IntegerField(default=0)
+    stock_minimo = models.IntegerField(default=5)
+    categoria = models.ForeignKey('Categoria', on_delete=models.SET_NULL, null=True)
+    imagen_url = models.CharField(max_length=255, blank=True, null=True)
+    activo = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'Producto'
+
