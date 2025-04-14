35665557003adcf480d4b14ee24863efe1f961b4
Javier Cabrera <fcabreraclaros@gmail.com>
2025-04-13 21:05:11 -0400

    procfile added

--- pos_backend/settings.py
+++ pos_backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-+y0nt%)=00q$$l-9_slepaaiim-)awd$@2zz%5)jo%j1y@ct-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
